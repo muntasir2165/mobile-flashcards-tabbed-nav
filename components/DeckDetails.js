2ef752ea3e2d4a143053234403ab132074d6e679
Muntasir Biojid <muntasir2165@hotmail.com>
2019-07-27 21:29:10 -0700

    Updated display of deck listing based on the number of decks in the listing

--- components/DeckDetails.js
+++ components/DeckDetails.js
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
+import { saveDecks, fetchDecks } from "../utils/api";
 
 class DeckDetails extends Component {
+  state = {
+    deckListing: {}
+  };
+  componentDidMount() {
+    fetchDecks()
+      .then(decks => this.setState({ deckListing: decks }))
+      .catch(error => console.log(error));
+  }
+
+  deleteDeck = deckId => {
+    // console.log("Id of the deck to delete: " + deckId);
+    // console.log("Current decks: " + JSON.stringify(this.state.deckListing));
+    // const currentDecks = { ...this.state.deckListing };
+    // currentDecks[deckId] = undefined;
+    // delete currentDecks[deckId];
+    // console.log("Decks after the delete: " + JSON.stringify(currentDecks));
+    // saveDecks(currentDecks)
+    //   .then(() => this.props.navigation.navigate("DeckListing"))
+    //   .catch(error => console.log(error));
+  };
+
   render() {
     return (
-      <View>
-        <Text style={{ fontSize: 20 }}>{this.props.navigation.state.params.deck.title}</Text>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <TouchableOpacity onPress={() => {}}>
+          <Text>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}}>
+          <Text>Start Quiz</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() =>
+            this.deleteDeck(this.props.navigation.state.params.deckId)
+          }
+        >
+          <Text>Delete Deck</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
-
-const mapStateToProps = deckList => ({});
-
-const mapDispatchToProps = dispatch => ({});
 
 export default DeckDetails;
--- components/DeckListing.js
+++ components/DeckListing.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Text, Button } from "react-native";
 import { saveDecks, fetchDecks } from "../utils/api";
 import { defaultDecks } from "../utils/defaultDecks";
 
@@ -7,10 +7,20 @@ class DeckListing extends Component {
   state = {
     deckListing: {}
   };
+
+  // Used for debugging purposes
+  componentDidMount1() {
+    saveDecks({});
+  }
+
   componentDidMount() {
     fetchDecks()
       .then(decks => {
-        if (!JSON.stringify(decks)) {
+        console.log("Current decks in AsyncStorage: " + JSON.stringify(decks));
+        if (
+          !JSON.stringify(decks) ||
+          Object.keys(JSON.parse(decks)).length === 0
+        ) {
           console.log("Setting up AsyncStorage with default decks");
           saveDecks(defaultDecks)
             .then(() => console.log("Updated AsyncStorage with default decks"))
@@ -35,36 +45,32 @@ class DeckListing extends Component {
   render() {
     return (
       <View
-        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
       >
-        {Object.keys(this.state.deckListing).map((deckId, index) => (
-          <Button
-            key={`${index}-${this.state.deckListing[deckId]}`}
-            style={styles.deckButton}
-            title={`${this.state.deckListing[deckId].title}\n${
-              this.state.deckListing[deckId].questions.length
-            } cards`}
-            onPress={() =>
-              this.props.navigation.navigate("DeckDetails", {
-                deck: this.state.deckListing[deckId]
-              })
-            }
-          />
-        ))}
+        {Object.keys(this.state.deckListing).length > 0 ? (
+          Object.keys(this.state.deckListing).map((deckId, index) => (
+            <Button
+              key={`${index}-${this.state.deckListing[deckId]}`}
+              title={`${this.state.deckListing[deckId].title}\n${
+                this.state.deckListing[deckId].questions.length
+              } cards`}
+              onPress={() =>
+                this.props.navigation.navigate("DeckDetails", {
+                  deckId: deckId
+                })
+              }
+            />
+          ))
+        ) : (
+          <Text>No Deck to Display</Text>
+        )}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  deckButton: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 20
-  }
-});
-
 
 export default DeckListing;
